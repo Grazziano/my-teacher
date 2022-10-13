8e8db2da8a0db7e77a3b0b63e38214f393c549eb
Grazziano Fagundes <grazzianofagundes@gmail.com>
2022-10-13 12:46:02 -0300

    Estilizando lista

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,8 +1,13 @@
+import { Box } from '@mui/material';
 import type { NextPage } from 'next';
 import List from '../src/components/List/List';
 
 const Home: NextPage = () => {
-  return <List />;
+  return (
+    <Box sx={{ backgroundColor: 'secondary.main' }}>
+      <List></List>
+    </Box>
+  );
 };
 
 export default Home;
--- frontend/src/components/List/List.tsx
+++ frontend/src/components/List/List.tsx
@@ -16,9 +16,39 @@ const List = () => {
         <Photo src="https://github.com/grazziano.png"></Photo>
         <Information>
           <Name>Grazziano Fagundes</Name>
-          <Value>R$ 100,00</Value>
+          <Value>R$ 100,00 por hora</Value>
           <Description>Aulas de Programação</Description>
-          <Button>Marcar Aula</Button>
+          <Button sx={{ width: '70%' }}>Marcar Aula com Grazziano</Button>
+        </Information>
+      </ListItem>
+
+      <ListItem>
+        <Photo src="https://github.com/grazziano.png"></Photo>
+        <Information>
+          <Name>Grazziano Fagundes</Name>
+          <Value>R$ 100,00 por hora</Value>
+          <Description>Aulas de Programação</Description>
+          <Button sx={{ width: '70%' }}>Marcar Aula com Grazziano</Button>
+        </Information>
+      </ListItem>
+
+      <ListItem>
+        <Photo src="https://github.com/grazziano.png"></Photo>
+        <Information>
+          <Name>Grazziano Fagundes</Name>
+          <Value>R$ 100,00 por hora</Value>
+          <Description>Aulas de Programação</Description>
+          <Button sx={{ width: '70%' }}>Marcar Aula com Grazziano</Button>
+        </Information>
+      </ListItem>
+
+      <ListItem>
+        <Photo src="https://github.com/grazziano.png"></Photo>
+        <Information>
+          <Name>Grazziano Fagundes</Name>
+          <Value>R$ 100,00 por hora</Value>
+          <Description>Aulas de Programação</Description>
+          <Button sx={{ width: '70%' }}>Marcar Aula com Grazziano</Button>
         </Information>
       </ListItem>
     </ListStyled>
