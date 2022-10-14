d7f24480bac0112b5c65cfda34f234c47a9d45bf
Grazziano Fagundes <grazzianofagundes@gmail.com>
2022-10-14 14:59:51 -0300

    Criando view de cadastro de aulas

--- backend/teacher/serializer.py
+++ backend/teacher/serializer.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from teacher.models import Teacher
+from teacher.models import Teacher, Aula
 
 
 class TeacherSerializer(serializers.ModelSerializer):
@@ -7,3 +7,14 @@ class TeacherSerializer(serializers.ModelSerializer):
         model = Teacher
         # fields = ('id', 'name', 'hour_value', 'description', 'photo')
         fields = ('__all__')
+
+
+class CadastrarAulaSerializer(serializers.Serializer):
+    email = serializers.EmailField(max_length=255)
+    name = serializers.CharField(max_length=100)
+
+
+class AulaSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Aula
+        fields = '__all__'
--- backend/teacher/views.py
+++ backend/teacher/views.py
@@ -1,9 +1,10 @@
+from django.shortcuts import get_object_or_404
 from rest_framework.views import APIView
 from rest_framework.views import Response
-from rest_framework.status import HTTP_200_OK
+from rest_framework.status import HTTP_200_OK, HTTP_201_CREATED, HTTP_400_BAD_REQUEST
 
-from teacher.models import Teacher
-from teacher.serializer import TeacherSerializer
+from teacher.models import Teacher, Aula
+from teacher.serializer import TeacherSerializer, CadastrarAulaSerializer, AulaSerializer
 
 
 class TeacherAPIView(APIView):
@@ -11,3 +12,20 @@ class TeacherAPIView(APIView):
         teachers = Teacher.objects.all()
         serializer = TeacherSerializer(teachers, many=True)
         return Response(serializer.data, status=HTTP_200_OK)
+
+
+class CadastrarAulaAPIView(APIView):
+    def post(self, request, id, format=None):
+        teacher = get_object_or_404(Teacher, id=id)
+        serializer = CadastrarAulaSerializer(data=request.data)
+
+        if serializer.is_valid():
+            aula = Aula(
+                name=serializer.validated_data.get('name'),
+                email=serializer.validated_data.get('email'),
+                teacher=teacher
+            )
+            aula.save()
+            aula_serializer = AulaSerializer(aula, many=False)
+            return Response(aula_serializer.data, status=HTTP_201_CREATED)
+        return Response(serializer.errors, status=HTTP_400_BAD_REQUEST)
