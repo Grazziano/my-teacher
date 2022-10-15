0c8f057ed395d18ca01326d5113ac72d1e6bea58
Grazziano Fagundes <grazzianofagundes@gmail.com>
2022-10-15 13:53:30 -0300

    Buscando os dados dinamicamente do backend

--- frontend/src/hooks/pages/useIndex.ts
+++ frontend/src/hooks/pages/useIndex.ts
@@ -1,37 +1,15 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Teacher } from '../../@types/teacher';
+import { ApiService } from '../../services/ApiService';
 
 export function useIndex() {
-  const [teachersList, setTeachersList] = useState<Teacher[]>([
-    {
-      id: 1,
-      name: 'Professor 1',
-      photo: 'https://github.com/grazziano.png',
-      description: 'Descrição do professor 1',
-      hour_value: 100,
-    },
-    {
-      id: 2,
-      name: 'Professor 2',
-      photo: 'https://github.com/grazziano.png',
-      description: 'Descrição do professor 2',
-      hour_value: 200,
-    },
-    {
-      id: 3,
-      name: 'Professor 3',
-      photo: 'https://github.com/grazziano.png',
-      description: 'Descrição do professor 3',
-      hour_value: 300,
-    },
-    {
-      id: 4,
-      name: 'Professor 4',
-      photo: 'https://github.com/grazziano.png',
-      description: 'Descrição do professor 4',
-      hour_value: 400,
-    },
-  ]);
+  const [teachersList, setTeachersList] = useState<Teacher[]>([]);
+
+  useEffect(() => {
+    ApiService.get('/professores').then((response) => {
+      setTeachersList(response.data);
+    });
+  }, []);
 
   return {
     teachersList,
